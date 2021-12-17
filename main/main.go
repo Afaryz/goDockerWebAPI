5dd97c76459290bf98091ee497f3b15ffba41f0c
Afaryz <69769189+Afaryz@users.noreply.github.com>
2021-12-17 22:59:04 +0100

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	request1()
+}
+
+func homePage(response http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Welcome to the Go Web API")
+	fmt.Println("Endpoint Hit: homepage")
+}
+
+func aboutMe(response http.ResponseWriter, r *http.Request) {
+	who := "Kevin Kowalski"
+
+	fmt.Fprintf(response, "A little about...")
+	fmt.Println("Endpoint Hit: ", who)
+
+}
+func request1() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/aboutme", aboutMe)
+}
